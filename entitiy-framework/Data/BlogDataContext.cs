3b77c05fa2949a41a559aa69507fefe89ea35bcd
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-05-01 10:13:39 -0300

    Data do entity framework criado

---
+++ entitiy-framework/Data/BlogDataContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using entitiy_framework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace entitiy_framework.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server = localhost, 1433; Database = Blog; User ID = sa; Password = 1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+
+    }
+}
