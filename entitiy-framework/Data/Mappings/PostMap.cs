f28bcd8c3eed3eb09e88f968814294077c5b9886
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-05-06 13:50:21 -0300

    Criando a API

--- entitiy-framework/Data/Mappings/PostMap.cs
+++ entitiy-framework/Data/Mappings/PostMap.cs
@@ -38,6 +38,30 @@ namespace entitiy_framework.Data.Mappings
                 .HasDefaultValue(DateTime.Now.ToUniversalTime());
             builder.HasIndex(x => x.Slug, "IX_PostSlug")
                 .IsUnique();
+
+            // relacionamento
+            builder.HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_PostAuthor")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // relacionamento muito p muitos
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<String, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                    .WithMany()
+                    .HasForeignKey("PostId")
+                    .HasConstraintName("FK_PostTag_PostId")
+                    .OnDelete(DeleteBehavior.Restrict),
+                    tag => tag.HasOne<Post>()
+                    .WithMany()
+                    .HasForeignKey("TagId")
+                    .HasConstraintName("FK_PostTag_TagId")
+                    .OnDelete(DeleteBehavior.Restrict)
+                );
+
         }
     }
 }
